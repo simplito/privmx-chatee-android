07267b25277f10efaa3d815a8b6ccde8f2297da2
djenczewski <dawid.jenczewski.dj@gmail.com>
2024-11-05 14:12:06 +0100

    chore: update version of privmx-endpoint-install-native plugin to "1.1"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 10
-        versionName = "2.0.0"
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
